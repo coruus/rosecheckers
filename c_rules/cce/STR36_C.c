de10cf3d269562decf02724c07e4199ec51ecdb5
avolkovi <avolkovi@a2ac0d72-06d8-44ab-9e58-0f2374626789>
2008-06-11 16:34:09 +0000

    STR36
    git-svn-id: svn://svn.code.sf.net/p/rosecheckers/code/trunk@27 a2ac0d72-06d8-44ab-9e58-0f2374626789

---
+++ c_rules/cce/STR36_C.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+	char s[] = "abc";
+	printf("%s\n", s);
+	return 0;
+}
---
+++ c_rules/ncce/STR36_C.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+	char s[3] = "abc";
+	printf("%s\n", s);
+	return 0;
+}
