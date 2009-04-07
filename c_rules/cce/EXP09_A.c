53cd6417906cebe859a21cd04201ee6ba8aebeb5
svoboda <svoboda@a2ac0d72-06d8-44ab-9e58-0f2374626789>
2009-04-07 18:14:15 +0000

    EXP09 code matches slides
    git-svn-id: svn://svn.code.sf.net/p/rosecheckers/code/branches/homework@185 a2ac0d72-06d8-44ab-9e58-0f2374626789

--- c_rules/cce/EXP09_A.c
+++ c_rules/cce/EXP09_A.c
@@ -3,14 +3,14 @@
 
 void EXP09_A() {
   size_t i;
-  int **triarray = calloc(100, sizeof(int *));
+  int **triarray = (int**) calloc(100, sizeof(int *));
 
   if (!triarray) {
     /* handle error */
   }
 
   for (i = 0; i < 100; i++) {
-    triarray[i] = calloc(i, sizeof(int));
+    triarray[i] = (int*) calloc(i, sizeof(int));
     if (!triarray[i]) {
       /* handle error */
     }
