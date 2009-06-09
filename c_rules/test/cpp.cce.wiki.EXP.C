a780c303a348b121462a4a6dabcfc43ceb3bbaab
jpincar <jpincar@a2ac0d72-06d8-44ab-9e58-0f2374626789>
2009-06-09 14:37:22 +0000

    Added EXP CPP tests.
    git-svn-id: svn://svn.code.sf.net/p/rosecheckers/code/trunk@270 a2ac0d72-06d8-44ab-9e58-0f2374626789

---
+++ c_rules/test/cpp.cce.wiki.EXP.C
@@ -0,0 +1,293 @@
+/*
+ * Copyright (c) 2007 Carnegie Mellon University.
+ * All rights reserved.
+
+ * Permission to use this software and its documentation for any purpose is hereby granted,
+ * provided that the above copyright notice appear and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that the name of CMU not
+ * be used in advertising or publicity pertaining to distribution of the software without
+ * specific, written prior permission.
+ *
+ * CMU DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL CMU BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, RISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "util.h"
+#include "cpp.util.h"
+
+void EXP00();
+void EXP01();
+void EXP02();
+void EXP05();
+void EXP06();
+void EXP07();
+void EXP09();
+void EXP10();
+void EXP11();
+void EXP12();
+void EXP15();
+void EXP17();
+void EXP31();
+void EXP32();
+void EXP33();
+void EXP34();
+void EXP35();
+
+void EXP() {
+  EXP00();
+  EXP01();
+  EXP02();
+  EXP05();
+  EXP06();
+  EXP07();
+  EXP09();
+  EXP10();
+  EXP11();
+  EXP12();
+  EXP15();
+  EXP17();
+  EXP31();
+  EXP32();
+  EXP33();
+  EXP34();
+  EXP35();
+}
+
+void EXP00() {
+  const int x = random();
+
+  if ((x & 1) == 0) {
+    printf("EXP00");
+  }
+}
+
+enum { SIZE_MAX = 256 };
+
+double *EXP01_allocate_array(size_t num_elems) {
+  double *d_array;
+
+  if (num_elems > SIZE_MAX/sizeof(*d_array)) {
+    /* handle error condition */
+  }
+  d_array = (double *)malloc(sizeof(*d_array) * num_elems);
+  if (d_array == NULL) {
+    /* handle error condition */
+  }
+  return d_array;
+}
+
+void EXP01() {
+  free(EXP01_allocate_array(3));
+}
+
+
+void EXP02() {
+  char *p = NULL;
+
+  const unsigned int BUF_SIZE = 12;
+
+  if (p == NULL) p = (char *)malloc(BUF_SIZE);
+  if (p == NULL) {
+    /* handle error */
+    return;
+  }
+  
+  /* do stuff with p */
+  
+  free(p);
+}
+
+enum {EXP03_buffer_size = 50};
+
+struct EXP03_buffer {
+  size_t size;
+  char bufferC[EXP03_buffer_size];
+};
+
+void EXP03_func(const struct EXP03_buffer *buf) {
+
+  struct EXP03_buffer *buf_cpy = 
+    (struct EXP03_buffer *)malloc(sizeof(struct EXP03_buffer));
+
+  if (buf_cpy == NULL) {
+    /* Handle malloc() error */
+  }
+
+  /* ... */
+
+  memcpy(buf_cpy, buf, sizeof(struct EXP03_buffer));
+
+  /* ... */
+
+  free(buf_cpy);
+}
+
+class my_buf {
+public:
+  /* ... */
+  bool buf_compare(const my_buf *that);
+private:
+  char buff_type;
+  unsigned int size;
+  char buffer[50];
+};
+
+
+bool my_buf::buf_compare(const my_buf *that)
+{
+  if (this->buff_type != that->buff_type) return 0;
+  if (this->size != that->size) return 0;
+  if (strcmp(this->buffer, that->buffer) != 0) return 0;
+  return 1;
+}
+
+void EXP05() {
+  int dividend, divisor;
+  double result = static_cast<double>(dividend)/divisor;
+}
+
+
+void EXP06() {
+  int a = 14;
+  const int b = sizeof(a);
+  a++;
+  printf("EXP06 %d\n", b);
+}
+
+void EXP07() {
+  const unsigned int nbytes = 4;
+  const unsigned int nblocks = 1 + (nbytes - 1) / BUFSIZ;
+  printf("EXP07 %d\n", nblocks);
+}
+
+void EXP09() {
+  size_t i;
+  int **matrix = (int **)calloc(100, sizeof(*matrix));
+  if (matrix == NULL) {
+    /* handle error */
+  }
+
+  for (i = 0; i < 100; i++) {
+    matrix[i] = (int *)calloc(i, sizeof(**matrix));
+    if (matrix[i] == NULL) {
+      /* handle error */
+    }
+  }
+}
+
+int EXP10_g;
+
+int EXP10_f(int i) {
+  EXP10_g = i;
+  return i;
+}
+
+void EXP10() {
+  int x = EXP10_f(1);
+  x += EXP10_f(2); 
+}
+
+void EXP11() {
+  float f = 0.0;
+  int i = 0;
+  float *fp;
+  int *ip;
+
+  ip = &i;
+  fp = &f;
+  printf("int is %d, float is %f\n", i, f);
+  (*ip)++;
+  (*fp)++;
+
+  printf("int is %d, float is %f\n", i, f);
+}
+
+void EXP12() {
+  if (!(cout << "foo")) {
+    /* Handle Error */
+  }
+}
+
+void EXP15() {
+  uint8_t port = 0x5aU;
+  uint8_t result_8 = (static_cast<uint8_t> (~port)) >> 4;
+}
+
+void EXP17() {
+  int a,b,c;
+
+  if ( a < b && b < c ) {}
+
+  if ( a == b && a == c ){}
+}
+
+void EXP30() {
+  int i = 0;
+  i = i + 2;
+}
+
+void EXP31() {
+  unsigned int EXP31_index = 0;
+  assert(EXP31_index > 0);
+  EXP31_index++;
+}
+
+
+void EXP32() {
+  static volatile int **ipp;
+  static volatile int *ip;
+  static volatile int i = 0;
+
+  cout << "i = " << i << "." << endl;
+
+  ipp = &ip;
+  *ipp = &i;
+  if (*ip != 0) {
+    /* ... */
+  }
+}
+
+void EXP33_set_flag(int number, int *sign_flag) {
+  if (sign_flag == NULL) {
+    return;
+  }
+  if (number >= 0) {
+    *sign_flag = 1;
+  }
+  else if (number < 0) {
+    *sign_flag = -1;
+  }
+}
+
+void EXP33() {
+  int sign;
+  int number = 2;
+
+  EXP33_set_flag(number, &sign);
+
+  number = sign + 1;
+}
+
+
+void EXP34() {
+  char input_str[] = "foo";
+  const size_t size = strlen(input_str)+1;
+  char *str = (char*) malloc(size);
+  if(str == NULL) {
+    /* Handle allocation error */
+  }
+
+  memcpy(str, input_str, size);
+  /* ... */
+  free(str);
+  str = NULL; 
+}
+
+void EXP35() {
+  int vals[] = {3, 4, 5};
+  memset(vals, 0, sizeof(vals));
+}
+
---
+++ c_rules/test/cpp.ncce.wiki.EXP.C
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) 2007 Carnegie Mellon University.
+ * All rights reserved.
+
+ * Permission to use this software and its documentation for any purpose is hereby granted,
+ * provided that the above copyright notice appear and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that the name of CMU not
+ * be used in advertising or publicity pertaining to distribution of the software without
+ * specific, written prior permission.
+ *
+ * CMU DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL CMU BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, RISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "util.h"
+#include "cpp.util.h"
+
+void EXP00();
+void EXP01();
+void EXP02();
+void EXP03();
+void EXP04();
+void EXP05();
+void EXP06();
+void EXP07();
+void EXP09();
+void EXP10();
+void EXP11();
+void EXP12();
+void EXP15();
+void EXP17();
+void EXP30();
+void EXP31();
+void EXP33();
+void EXP34();
+void EXP35();
+
+void EXP() {
+  EXP00();
+  EXP01();
+  EXP02();
+  EXP03();
+  EXP04();
+  EXP05();
+  EXP06();
+  EXP07();
+  EXP09();
+  EXP10();
+  EXP11();
+  EXP12();
+  EXP15();
+  EXP17();
+  EXP30();
+  EXP31();
+  EXP33();
+  EXP34();
+  EXP35();
+}
+
+void EXP00() {
+  const int x = 0;
+
+  printf("EXP00 %d\n" ,x & 1 == 0);
+}
+
+enum { SIZE_MAX = 256 };
+
+double *EXP01_allocate_array(size_t num_elems) {
+  double *d_array;
+
+  if (num_elems > SIZE_MAX/sizeof(d_array)) {
+    /* handle error condition */
+  }
+  d_array = (double *)malloc(sizeof(d_array) * num_elems);
+  if (d_array == NULL) {
+    /* handle error condition */
+  }
+  return d_array;
+}
+
+void EXP01() {
+  free(EXP01_allocate_array(3));
+}
+
+void EXP02() {
+  char *p;
+
+  const unsigned int BUF_SIZE = 12;
+
+  if ( p || (p = (char *)malloc(BUF_SIZE)) ) {
+    /* do stuff with p */
+  }
+  else {
+    /* handle error */
+    return;
+  }
+}
+
+
+struct EXP03_buffer {
+  size_t size;
+  char bufferC[50];
+};
+
+void EXP03_func(const struct EXP03_buffer *buf) {
+
+  /* Assumes sizeof( struct buffer) =
+   * sizeof( size_t) + 50 * sizeof( char) = 54 */
+  struct EXP03_buffer *buf_cpy = (struct EXP03_buffer *)malloc(54);
+
+  if (buf_cpy == NULL) {
+    /* Handle malloc() error */
+  }
+
+  /*
+   * With padding, sizeof(struct buffer) may be greater than
+   * 54, causing some data to be written outside the bounds 
+   * of the memory allocated.
+   */
+  memcpy(buf_cpy, buf, sizeof(struct EXP03_buffer));
+
+  /* ... */
+
+  free(buf_cpy);
+}
+
+void EXP03() {
+  EXP03_func(NULL);
+}
+
+class my_buf {
+public:
+  // ...
+private:
+  char buff_type;
+  size_t size;
+  char buffer[50];
+};
+
+unsigned int EXP04_buf_compare(
+			 const my_buf *s1, 
+			 const my_buf *s2) 
+{
+  if (!memcmp(s1, s2, sizeof(my_buf))) {
+    return 1;
+  }
+  return 0;
+}
+
+void EXP04() {
+  if (EXP04_buf_compare(NULL, NULL) == 0) {}
+}
+
+void EXP05() {
+  int dividend, divisor;
+  // ...
+  double result = ((double)dividend)/divisor;
+}
+
+void EXP06() {
+  int a = 14;
+  const int b = sizeof(a++);
+  printf("EXP06 %d %d\n", a, b);
+}
+
+void EXP07() {
+  const unsigned int nbytes = 4;
+  const unsigned int nblocks = 1 + ((nbytes - 1) >> 9); /* BUFSIZ = 512 = 2^9 */
+  printf("EXP07 %d\n", nblocks);
+}
+
+void EXP09() {
+  size_t i;
+  int **matrix = (int **)calloc(100, 4);
+  if (matrix == NULL) {
+    /* handle error */
+  }
+
+  for (i = 0; i < 100; i++) {
+    matrix[i] = (int *)calloc(i, 4);
+    if (matrix[i] == NULL) {
+      /* handle error */
+    }
+  }
+}
+
+int EXP10_g;
+
+int EXP10_f(int i) {
+  EXP10_g = i;
+  return i;
+}
+
+void EXP10() {
+  const int x = EXP10_f(1) + EXP10_f(2); 
+}
+
+
+void EXP11() {
+  float f = 0.0;
+  int i = 0;
+  float *fp;
+  int *ip;
+
+  assert(sizeof(int) == sizeof(float));
+  ip = (int*) &f;
+  fp = (float*) &i;
+  printf("int is %d, float is %f\n", i, f);
+  (*ip)++;
+  (*fp)++;
+
+  printf("int is %d, float is %f\n", i, f);
+}
+
+void EXP12() {
+  cout << "foo";
+}
+
+void EXP15() {
+  uint8_t port = 0x5aU;
+  uint8_t result_8 = ( ~port ) >> 4;
+}
+
+void EXP17() {
+  int a = 2;
+  int b = 2;
+  int c = 2;
+
+  if (a < b < c) {}
+
+  if (a == b == c) {}
+}
+
+void EXP30() {
+  int i = 0;
+  i = ++i + i;
+}
+
+void EXP31() {
+  unsigned int EXP31_index = 0;
+  assert(EXP31_index++ > 0);
+}
+
+
+void EXP33_set_flag(int number, int *sign_flag) {
+  if (sign_flag == NULL) {
+    return;
+  }
+  if (number > 0) {
+    *sign_flag = 1;
+  }
+  else if (number < 0) {
+    *sign_flag = -1;
+  }
+}
+
+void EXP33() {
+  int sign;
+  int number = 2;
+
+  EXP33_set_flag(number, &sign);
+
+  number = sign + 1;
+}
+
+void EXP34() {
+  char *input_str = (char*) malloc(3);
+  const size_t size = strlen(input_str)+1;
+  char *str = (char*) malloc(size);
+  memcpy(str, input_str, size);
+  /* ... */
+  free(str);
+  str = NULL; 
+}
+
+
+void EXP35() {
+  int const vals[] = {3, 4, 5};
+  memset((int*) vals, 0, sizeof(vals));
+}
+
